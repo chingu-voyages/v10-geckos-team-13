fb145f1c3c09e502b2630bf9e219ef96ec2b5104
iamllino <llino.gadia.delrosario@gmail.com>
2019-07-06 19:06:39 +0800

    initial commit

---
+++ react-leaflet-starter/src/RestaurantData.js
@@ -0,0 +1,22 @@
+const restaurants = [
+  {
+    id: 1,
+    name: "restaurant name",
+    latitude: 51.513752,
+    longitude: -0.094736
+  },
+  {
+    id: 2,
+    name: "restaurant name",
+    latitude: 51.516561,
+    longitude: -0.077865
+  },
+  {
+    id: 3,
+    name: "restaurant name",
+    latitude: 51.519095,
+    longitude: -0.083451
+  }
+];
+
+export default restaurants;
