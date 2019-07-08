3a266f15e227b98dbfab251ce3500bc1032899a8
Cheah Chung Yin <cheahchungyin@gmail.com>
2019-07-09 04:20:04 +0800

    Add basic map, detect and set user location

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Map from './Map/Map';
+
 class App extends Component {
   render () {
     return (
       <div className="App">
-      <header className="App-header">
-        <h1>App works!</h1>
-      </header>
-    </div>
+        <Map />
+      </div>
     );
   }
 }
